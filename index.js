2c87b61c0c4de433e98efc24fe70892ecd056f48
Tabatha Slatton <valbatha@gmail.com>
2020-11-23 21:22:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+const createEmployeeRecord = employee => {
+    const [firstName, familyName, title, payPerHour] = employee
+    return {firstName: firstName, familyName: familyName, title: title, payPerHour: payPerHour, timeInEvents: [], timeOutEvents: []}
+
+}
+const createEmployeeRecords = employees => {
+    // employees.map(employee => createEmployeeRecord(employee))
+    const employeesAr = []
+    for(const employee in employee) {
+       employee.push(createEmployeeRecord(employee))
+    }
+}
