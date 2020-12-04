3e531e4921ca9595d1004641a036fd140d556b35
atBeatty <adam.t.beatt@gmail.com>
2020-12-04 15:20:21 -0800

    Done.

--- index.js
+++ index.js
@@ -21,15 +21,16 @@ function createEmployeeRecords(array){
 }
 
 function createTimeInEvent(record, time){
-    let hour = makeTimeFormat(time)
+    // let hour = makeTimeFormat(time)
     let hourInt = parseInt(time.split(" ")[1])
     let date = time.split(" ")[0]
-    let newDateString = `${date}T${hour}`
+    // let newDateString = `${date}T${hour}`
 
-    let newTimeStamp = new Date(newDateString)
-    newTimeStamp.type = "TimeIn"
-    newTimeStamp.hour = hourInt
-    newTimeStamp.date = date
+    let newTimeStamp = {
+        type: "TimeIn",
+        hour: hourInt,
+        date: date
+    }
     
     record["timeInEvents"].push(newTimeStamp)
 
@@ -37,15 +38,16 @@ function createTimeInEvent(record, time){
 }
 
 function createTimeOutEvent(record, time){
-    let hour = makeTimeFormat(time)
+    // let hour = makeTimeFormat(time)
     let hourInt = parseInt(time.split(" ")[1])
     let date = time.split(" ")[0]
-    let newDateString = `${date}T${hour}`
+    // let newDateString = `${date}T${hour}`
 
-    let newTimeStamp = new Date(newDateString)
-    newTimeStamp.type = "TimeOut"
-    newTimeStamp.hour = hourInt
-    newTimeStamp.date = date
+    let newTimeStamp = {
+        type: "TimeOut",
+        hour: hourInt,
+        date: date
+    }
     
     record["timeOutEvents"].push(newTimeStamp)
 
@@ -53,11 +55,15 @@ function createTimeOutEvent(record, time){
 }
 
 function hoursWorkedOnDate(record, date){
-    console.log(record)
-    
-    let timeIn = record.timeInEvents[0].hour
-    let timeOut = record.timeOutEvents[0].hour
-    return ((timeOut - timeIn)/100)
+    let timeInEvents = record.timeInEvents
+    let timeOutEvents = record.timeOutEvents
+    let timeIn = timeInEvents.find(el => el.date === date)
+    let timeOut = timeOutEvents.find(el => el.date === date)
+    // console.log(timeIn.hour, timeOut.hour)
+    // let timeIn = record.timeInEvents[0].hour
+    // let timeOut = record.timeOutEvents[0].hour
+    return ((timeOut.hour - timeIn.hour)/100)
+
 }
 
 function wagesEarnedOnDate(record, date){
@@ -68,32 +74,42 @@ function wagesEarnedOnDate(record, date){
 }
 
 function allWagesFor(record){
-    console.log(record)
+    let timeInEvents = record.timeInEvents
+    let allWages = 0
+    // let timeOutEvents = record.timeOutEvents
 
-    
+
+    timeInEvents.forEach(function(event){
+        allWages += wagesEarnedOnDate(record, event.date)
+        // console.log(wagesEarnedOnDate(record, event.date), allWages)
+    })
+    // console.log(allWages)
+    return allWages
 }
 
-function calculatePayroll(arr){
-    return arr.reduce(function(sum, el){
-        return sum + allWagesFor(el)
-    })
-
+function calculatePayroll(array){
+    return array.reduce(function(sum, currentEmployee){
+        // console.log(allWagesFor(currentEmployee), "YUIP")
+        console.log(sum, "SUM")
+        // console.log(currentEmployee, "CURRENT")
+        return sum + allWagesFor(currentEmployee)
+    }, 0)
 }
 
 function findEmployeeByFirstName(array, firstName){
     return array.find(el => el.firstName === firstName)
 }
 
-function makeTimeFormat(stringTime){
-    let returnStringTime
-    stringTime = stringTime.split(" ")[1]
-    if (stringTime.length === 4){
-      returnStringTime = `${stringTime[0]}${stringTime[1]}:${stringTime[2]}${stringTime[3]}:00`
-    } else {
-      returnStringTime = `${stringTime[0]}${stringTime[1]}:${stringTime[2]}${stringTime[3]}:00`
-    }
+// function makeTimeFormat(stringTime){
+//     let returnStringTime
+//     stringTime = stringTime.split(" ")[1]
+//     if (stringTime.length === 4){
+//       returnStringTime = `${stringTime[0]}${stringTime[1]}:${stringTime[2]}${stringTime[3]}:00`
+//     } else {
+//       returnStringTime = `${stringTime[0]}${stringTime[1]}:${stringTime[2]}${stringTime[3]}:00`
+//     }
 
-    return returnStringTime
-  }
+//     return returnStringTime
+//   }
 
 
